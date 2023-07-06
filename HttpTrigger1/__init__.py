a6a7039a06cd626ad41494108d9238d5ddfed524
Micheal Emmanuel (meeckeyTek) <69390115+meeckeytek@users.noreply.github.com>
2023-07-06 11:50:16 +0100

    Update __init__.py

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Welcome to meeckey workd.')
 
     name = req.params.get('name')
     if not name:
@@ -16,7 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. Welcome to meeckey workd.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
